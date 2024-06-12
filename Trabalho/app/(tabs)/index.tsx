3fe853894797f34e025972e23f8a851f215169b2
MiguelSerafim7 <miguel.serafim.ptal@gmail.com>
2024-06-12 17:49:53 +0000

    save trabalho

---
+++ Trabalho/app/(tabs)/index.tsx
@@ -0,0 +1,19 @@
+import { Image, StyleSheet, View } from 'react-native';
+import React from 'react'
+import Home from '../../components/home'
+
+
+export default function HomeScreen() {
+  return (
+    <View style={styles.container}>
+
+      </View>
+  );
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+  },
+  
+});
